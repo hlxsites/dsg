33869a185ddca55d94a5c5d5c420dbb2ed783f03
Charity Helms <chelms@adobe.com>
2023-03-01 12:56:04 -0500

    trying again with spaces

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -5,11 +5,11 @@
 */
 
 import {
-decorateMain,
+  decorateMain,
 } from '../../scripts/scripts.js';
 
 import {
-loadBlocks,
+  loadBlocks,
 } from '../../scripts/lib-franklin.js';
 
 /**
@@ -21,25 +21,25 @@ async function loadFragment(path) {
 if (path && path.startsWith('/')) {
     const resp = await fetch(`${path}.plain.html`);
     if (resp.ok) {
-        const main = document.createElement('main');
-        main.innerHTML = await resp.text();
-        decorateMain(main);
-        await loadBlocks(main);
-        return main;
+      const main = document.createElement('main');
+      main.innerHTML = await resp.text();
+      decorateMain(main);
+      await loadBlocks(main);
+      return main;
     }
-}
-return null;
+  }
+  return null;
 }
 
 export default async function decorate(block) {
-const link = block.querySelector('a');
-const path = link ? link.getAttribute('href') : block.textContent.trim();
-const fragment = await loadFragment(path);
-if (fragment) {
+  const link = block.querySelector('a');
+  const path = link ? link.getAttribute('href') : block.textContent.trim();
+  const fragment = await loadFragment(path);
+  if (fragment) {
     const fragmentSection = fragment.querySelector(':scope .section');
     if (fragmentSection) {
         block.closest('.section').classList.add(...fragmentSection.classList);
         block.closest('.fragment-wrapper').replaceWith(...fragmentSection.childNodes);
     }
-}
+  }
 }
--- blocks/parallax/parallax.js
+++ blocks/parallax/parallax.js
@@ -3,10 +3,10 @@
 */
 
 function parallax(block, multiplier = 0.9) {
-window.addEventListener('scroll', () => {
+  window.addEventListener('scroll', () => {
     const offset = window.scrollY;
     block.style.backgroundPositionY = `${offset * multiplier}px`;
-});
+  });
 }
 
 /**
@@ -14,29 +14,27 @@ window.addEventListener('scroll', () => {
 * @param {Element} block The block
 */
 export default async function decorate(block) {
-const columns = [...block.children];
-const images = columns[1].querySelectorAll(':scope > div');
-const [desktopImage] = images[0].querySelector('img').src.split('?');
-let multiplier = 0.4;
-//TODO: finish mobile image or delete
-let mobileImage;
-if (images.length > 1) {
+  const columns = [...block.children];
+  const images = columns[1].querySelectorAll(':scope > div');
+  const [desktopImage] = images[0].querySelector('img').src.split('?');
+  let multiplier = 0.4;
+  // TODO: finish mobile image or delete
+  let mobileImage;
+  if (images.length > 1) {
     [mobileImage] = images[1].querySelector('img').src.split('?');
-}
+  }
 
-const parallaxBlock = document.createElement('div');
-parallaxBlock.classList.add('parallax-background');
-parallaxBlock.append(columns[0].querySelector(':scope > div'));
+  const parallaxBlock = document.createElement('div');
+  parallaxBlock.classList.add('parallax-background');
+  parallaxBlock.append(columns[0].querySelector(':scope > div'));
 
-parallaxBlock.style.backgroundImage = `url(${desktopImage})`;
-if (window.innerWidth < 900) {
+  parallaxBlock.style.backgroundImage = `url(${desktopImage})`;
+  if (window.innerWidth < 900) {
     parallaxBlock.style.backgroundImage = `url(${mobileImage})`;
     multiplier = 0.2;
 }
 
-block.textContent = '';
-block.append(parallaxBlock);
-
-parallax(parallaxBlock, multiplier);
-
+  block.textContent = '';
+  block.append(parallaxBlock);
+  parallax(parallaxBlock, multiplier);
 }
