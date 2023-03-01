52502a794430110cc76a3fad08ef54739a61725d
Charity Helms <chelms@adobe.com>
2023-03-01 12:45:57 -0500

    reducing spaces for linter

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -1,45 +1,45 @@
 /*
- * Fragment Block
- * Include content from one Helix page in another.
- * https://www.hlx.live/developer/block-collection/fragment
- */
+* Fragment Block
+* Include content from one Helix page in another.
+* https://www.hlx.live/developer/block-collection/fragment
+*/
 
 import {
-    decorateMain,
+decorateMain,
 } from '../../scripts/scripts.js';
 
 import {
-    loadBlocks,
+loadBlocks,
 } from '../../scripts/lib-franklin.js';
 
 /**
- * Loads a fragment.
- * @param {string} path The path to the fragment
- * @returns {HTMLElement} The root element of the fragment
- */
+* Loads a fragment.
+* @param {string} path The path to the fragment
+* @returns {HTMLElement} The root element of the fragment
+*/
 async function loadFragment(path) {
-    if (path && path.startsWith('/')) {
-        const resp = await fetch(`${path}.plain.html`);
-        if (resp.ok) {
-            const main = document.createElement('main');
-            main.innerHTML = await resp.text();
-            decorateMain(main);
-            await loadBlocks(main);
-            return main;
-        }
+if (path && path.startsWith('/')) {
+    const resp = await fetch(`${path}.plain.html`);
+    if (resp.ok) {
+        const main = document.createElement('main');
+        main.innerHTML = await resp.text();
+        decorateMain(main);
+        await loadBlocks(main);
+        return main;
     }
-    return null;
+}
+return null;
 }
 
 export default async function decorate(block) {
-    const link = block.querySelector('a');
-    const path = link ? link.getAttribute('href') : block.textContent.trim();
-    const fragment = await loadFragment(path);
-    if (fragment) {
-        const fragmentSection = fragment.querySelector(':scope .section');
-        if (fragmentSection) {
-            block.closest('.section').classList.add(...fragmentSection.classList);
-            block.closest('.fragment-wrapper').replaceWith(...fragmentSection.childNodes);
-        }
+const link = block.querySelector('a');
+const path = link ? link.getAttribute('href') : block.textContent.trim();
+const fragment = await loadFragment(path);
+if (fragment) {
+    const fragmentSection = fragment.querySelector(':scope .section');
+    if (fragmentSection) {
+        block.closest('.section').classList.add(...fragmentSection.classList);
+        block.closest('.fragment-wrapper').replaceWith(...fragmentSection.childNodes);
     }
+}
 }
